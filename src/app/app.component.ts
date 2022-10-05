444a7e43d4edbe76bec94ce14c38feafb0f0301c
jsonroa <70734005+jsonroa@users.noreply.github.com>
2022-10-05 17:56:18 -0500

    button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'hola soy json';
+  person = {
+    title = 'hola soy json',
+    name = 'Json',
+    age = 18
+  }
+  btnDisabled = false;
+  toggleButton(){
+    this.btnDisabled = true;
+  }
+  
+
 }
